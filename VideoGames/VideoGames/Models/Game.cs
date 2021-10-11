8a6d6e8894cf96852bed3d38035bcc4b78f9776b
Jonathon <jonathonq02@gmail.com>
2021-10-11 17:22:40 -0500

    Added in requirements for each field

--- VideoGames/VideoGames/Models/Game.cs
+++ VideoGames/VideoGames/Models/Game.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,10 +10,19 @@ namespace VideoGames.Models
 	public class Game
 	{
 		public int ID { get; set; }
+
+		[StringLength(60, MinimumLength = 3)]
+		[Required]
 		public string Name { get; set; }
 
+		[StringLength(60, MinimumLength = 3)]
+		[Required]
 		public string GameType { get; set; }
+
+		[StringLength(15, MinimumLength = 2)]
+		[Required]
 		public string Device { get; set; }
+
 		public bool LocalMultiplayer { get; set; }
 	}
 }
